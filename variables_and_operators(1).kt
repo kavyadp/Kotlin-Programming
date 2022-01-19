124c0749b0e6d46c15084a135e3bd9f624d5bbf2
Kavya D P <67356767+kavyadp@users.noreply.github.com>
2022-01-19 12:36:05 +0530

    Add files via upload

---
+++ variables_and_operators(1).kt
@@ -0,0 +1,19 @@
+fun main() {
+    val a = 2500
+    val b = 250
+
+    val sum = a + b
+    println(sum)
+
+    val diff = a - b
+    println(diff)
+
+    val product = a * b
+    println(product)
+
+    val quotient = a / b
+    println(quotient)
+
+
+
+}
